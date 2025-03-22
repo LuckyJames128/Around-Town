80480b2e8a9ef73bac34a4cb7a155c5d0cd3423d
LuckyJames128 <166640136+LuckyJames128@users.noreply.github.com>
2025-03-22 11:02:08 -0700

    Add files via upload

--- player.gd
+++ player.gd
@@ -1,18 +1,21 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 200.0
+const JUMP_VELOCITY = -250.0
+const gravity = 0
 
-
+func jump(): 
+	if is_on_floor():
+		velocity.y = JUMP_VELOCITY
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if Input.is_action_pressed("ui_up") or Input.is_action_pressed("ui_select"):
+		jump()
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
