033ae78d142aa5cfbddd797142655fa6ca244f02
LuckyJames128 <166640136+LuckyJames128@users.noreply.github.com>
2025-03-22 14:19:11 -0700

    Add files via upload

--- player.gd
+++ player.gd
@@ -1,36 +1,48 @@
 extends CharacterBody2D
 
 
+
 const SPEED = 200.0
 const JUMP_VELOCITY = -250.0
-const gravity = 0
-
+var stop = false
 func jump(): 
 	if is_on_floor():
 		velocity.y = JUMP_VELOCITY
 func _physics_process(delta: float) -> void:
-	$AnimatedSprite2D.play()
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+	if stop == false:
+		$AnimatedSprite2D.play()
+		# Add the gravity.
+		if not is_on_floor():
+			velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_pressed("ui_up") or Input.is_action_pressed("ui_select"):
-		jump()
+		# Handle jump.
+		if Input.is_action_pressed("ui_up") or Input.is_action_pressed("ui_select"):
+			jump()
 
-	if Input.is_action_just_pressed("ui_down") or Input.is_action_just_pressed("ui_select"):
-		var actionables = $actionable_finder.get_overlapping_areas()
-		if actionables.size() > 0:
-			actionables[0].action()
-			return
+		if Input.is_action_just_pressed("ui_down") or Input.is_action_just_pressed("ui_select"):
+			stop = true
+			var actionables = $actionable_finder.get_overlapping_areas()
+			if actionables.size() > 0:
+				stop = true
+				actionables[0].action()
+				return
+				
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-		$AnimatedSprite2D.animation = "run"
-		$AnimatedSprite2D.flip_h = direction<0
+		# Get the input direction and handle the movement/deceleration.
+		# As good practice, you should replace UI actions with custom gameplay actions.
+		var direction := Input.get_axis("ui_left", "ui_right")
+		if direction:
+			velocity.x = direction * SPEED
+			$AnimatedSprite2D.animation = "run"
+			$AnimatedSprite2D.flip_h = direction < 0
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+		move_and_slide()
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-	move_and_slide()
+		pass
+
+
+
+
+func _on_dialogue_label_finished_typing() -> void:
+	stop = false
