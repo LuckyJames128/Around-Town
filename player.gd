3213b2cbd085935722424a7db605917e80ac2389
LuckyJames128 <166640136+LuckyJames128@users.noreply.github.com>
2025-03-22 15:28:31 -0700

    Add files via upload

--- player.gd
+++ player.gd
@@ -1,35 +1,22 @@
 extends CharacterBody2D
 
-
-
 const SPEED = 200.0
 const JUMP_VELOCITY = -250.0
-var stop = false
+var stop
+
 func jump(): 
 	if is_on_floor():
 		velocity.y = JUMP_VELOCITY
 func _physics_process(delta: float) -> void:
-	if stop == false:
-		$AnimatedSprite2D.play()
-		# Add the gravity.
-		if not is_on_floor():
-			velocity += get_gravity() * delta
-
-		# Handle jump.
-		if Input.is_action_pressed("ui_up") or Input.is_action_pressed("ui_select"):
-			jump()
-
-		if Input.is_action_just_pressed("ui_down") or Input.is_action_just_pressed("ui_select"):
-			stop = true
-			var actionables = $actionable_finder.get_overlapping_areas()
-			if actionables.size() > 0:
-				stop = true
-				actionables[0].action()
-				return
-				
-
-		# Get the input direction and handle the movement/deceleration.
-		# As good practice, you should replace UI actions with custom gameplay actions.
+	$AnimatedSprite2D.play()
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+	# Handle jump.
+	if Input.is_action_pressed("ui_up"):
+		jump()
+			
+	if stop != true:
 		var direction := Input.get_axis("ui_left", "ui_right")
 		if direction:
 			velocity.x = direction * SPEED
@@ -38,11 +25,15 @@ func _physics_process(delta: float) -> void:
 		else:
 			velocity.x = move_toward(velocity.x, 0, SPEED)
 		move_and_slide()
-	else:
-		pass
 
+	if Input.is_action_just_pressed("ui_down"):
+		var actionables = $actionable_finder.get_overlapping_areas()
+		if actionables.size() > 0:
+			actionables[0].action()
+			return
 
-
-
-func _on_dialogue_label_finished_typing() -> void:
+func _on_panel_hidden():
 	stop = false
+
+func _on_panel_draw():
+	stop = true
