39e67ca202961af5d4ec566df6a2bae61caf33a7
LuckyJames128 <166640136+LuckyJames128@users.noreply.github.com>
2025-03-22 20:48:55 -0700

    bug fix

--- player.gd
+++ player.gd
@@ -11,23 +11,21 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 	# Handle jump.
-	if Input.is_action_pressed("ui_up") or Input.is_action_pressed("ui_select"):
+	if Input.is_action_pressed("ui_up"):
 		jump()
-	if Input.is_action_just_pressed("ui_down") or Input.is_action_just_pressed("ui_select"):
+	if Input.is_action_just_pressed("ui_down"):
 		var actionables = $actionable_finder.get_overlapping_areas()
 		if actionables.size() > 0:
 			actionables[0].action()
 			return
-
 		# Get the input direction and handle the movement/deceleration.
 		# As good practice, you should replace UI actions with custom gameplay actions.
-		var direction := Input.get_axis("ui_left", "ui_right")
-		if direction:
-			velocity.x = direction * SPEED
-			$AnimatedSprite2D.animation = "run"
-			$AnimatedSprite2D.flip_h = direction < 0
-		else:
-			velocity.x = move_toward(velocity.x, 0, SPEED)
-		move_and_slide()
+	var direction := Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+		$AnimatedSprite2D.animation = "run"
+		$AnimatedSprite2D.flip_h = direction < 0
 	else:
-		pass
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	move_and_slide()
+	
