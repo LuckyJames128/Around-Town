62acf89f3bbeac2e4aa09b0ac3266ac248b9e58a
ethan092609 <on6829et0926@gmail.com>
2025-03-24 03:10:53 -0700

    update 3:10 am

--- player.gd
+++ player.gd
@@ -1,10 +1,22 @@
 extends CharacterBody2D
 
+@onready var actionable_finder: Area2D = $Direction/ActionableFinder
+
+func _unhandled_input(_event:InputEvent) -> void:
+	#if Input.is_action_just_pressed("ui_accept"):
+		#DialogueManager.show_example_dialogue_balloon (load("res://dialogue/main.dialogue"), "start")
+	if Input.is_action_just_pressed("ui_accept"):
+		var actionables = actionable_finder.get_overlapping_areas()
+		if actionables.size() > 0:
+			actionables[0].action()
+			return
+
 const SPEED = 200.0
 const JUMP_VELOCITY = -250.0
 func jump(): 
 	if is_on_floor():
 		velocity.y = JUMP_VELOCITY
+
 func _physics_process(delta: float) -> void:
 	$AnimatedSprite2D.play()
 	# Add the gravity.
@@ -13,11 +25,8 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_pressed("ui_up"):
 		jump()
-	if Input.is_action_just_pressed("ui_down"):
-		var actionables = $actionable_finder.get_overlapping_areas()
-		if actionables.size() > 0:
-			actionables[0].action()
-			return
+	
+		
 		# Get the input direction and handle the movement/deceleration.
 		# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
