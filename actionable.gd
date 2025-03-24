f23648254fc124c4308a1b141db3c18248678bd8
LuckyJames128 <166640136+LuckyJames128@users.noreply.github.com>
2025-03-23 17:47:35 -0700

    Add files via upload

--- actionable.gd
+++ actionable.gd
@@ -2,6 +2,5 @@ extends Area2D
 
 @export var dialogue_resource = DialogueResource
 @export var dialogue_start = "start"
-
 func action() -> void:
 	DialogueManager.show_example_dialogue_balloon(dialogue_resource, dialogue_start)
